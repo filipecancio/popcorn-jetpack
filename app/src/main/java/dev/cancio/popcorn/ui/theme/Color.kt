1f95697d6c8394b9181cb4f6a428a8bf4aa0f6ce
cancio <filipe.cancio@outlook.com>
2021-08-13 01:18:16 -0300

    New colors pallete

--- app/src/main/java/dev/cancio/popcorn/ui/theme/Color.kt
+++ app/src/main/java/dev/cancio/popcorn/ui/theme/Color.kt
@@ -6,3 +6,13 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+
+val Green01 = Color(0xFF06948E)
+val Green02 = Color(0xFF034C49)
+val Green03 = Color(0xFF03403D)
+val Yellow = Color(0xFFFCD116)
+val Grey = Color(0xFFEFEFEF)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+
