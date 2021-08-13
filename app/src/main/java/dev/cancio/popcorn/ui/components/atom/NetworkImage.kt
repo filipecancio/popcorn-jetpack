af43c22274d7508a0f659e3bcce32bf7606d9a4f
cancio <filipe.cancio@outlook.com>
2021-08-13 09:14:26 -0300

    Create componente: Movie Item

---
+++ app/src/main/java/dev/cancio/popcorn/ui/components/atom/NetworkImage.kt
@@ -0,0 +1,49 @@
+package dev.cancio.popcorn.ui.components.atom
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
+import dev.cancio.popcorn.R
+
+
+@Composable
+fun NetworkImage(
+    url: String,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    contentScale: ContentScale = ContentScale.Crop,
+    placeholderColor: Color? = MaterialTheme.colors.onSurface
+) {
+    Box(modifier) {
+        val painter = rememberImagePainter(
+            data = url,
+            builder = {
+                placeholder(drawableResId = R.drawable.cruella)
+            }
+        )
+
+        Image(
+            painter = painter,
+            contentDescription = contentDescription,
+            contentScale = contentScale,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        if (painter.state is ImagePainter.State.Loading && placeholderColor != null) {
+            Spacer(
+                modifier = Modifier
+                    .matchParentSize()
+                    .background(placeholderColor)
+            )
+        }
+    }
+}
--- app/src/main/java/dev/cancio/popcorn/ui/components/molecule/MovieItem.kt
+++ app/src/main/java/dev/cancio/popcorn/ui/components/molecule/MovieItem.kt
@@ -3,6 +3,10 @@ package dev.cancio.popcorn.ui.components.atom
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -11,52 +15,97 @@ import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.cancio.popcorn.R
+import dev.cancio.popcorn.model.Movie
 import dev.cancio.popcorn.ui.theme.Green02
 import dev.cancio.popcorn.ui.theme.Grey
 import dev.cancio.popcorn.ui.theme.Yellow
 
-@Preview(showBackground = true)
+val example = Movie(
+    adult = false,
+    backdropPath = "/jlGmlFOcfo8n5tURmhC7YVd4Iyy.jpg",
+    genreIds = listOf(28, 12, 35, 878),
+    id = 436969,
+    originalLanguage = "en",
+    originalTitle = "The Suicide Squad",
+    overview = "Supervillains Harley Quinn, Bloodsport, Peacemaker and a collection of nutty cons at Belle Reve prison join the super-secret, super-shady Task Force X as they are dropped off at the remote, enemy-infused island of Corto Maltese.",
+    popularity = 5835.528,
+    posterPath = "/kb4s0ML0iVZlG6wAKbbs9NAm6X.jpg",
+    releaseDate = "2021-07-28",
+    title = "The Suicide Squad",
+    video = false,
+    voteAverage = 8.1,
+    voteCount = 1424
+)
+
+@Preview()
 @Composable
-fun MovieItem() {
-    Row(
+fun MovieItem(movie: Movie = example) {
+    Card(
+        elevation = 4.dp,
+        shape = RoundedCornerShape(20.dp),
+        backgroundColor = Green02,
+        contentColor = Grey,
         modifier = Modifier
-            .height(160.dp)
-            .background(Green02),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.cruella),
-            contentDescription = "cruella",
-            modifier = Modifier
-                .size(100.dp)
-        )
-        Column(
-            verticalArrangement = Arrangement.SpaceAround
-        ) {
-            Text(
-                "Cruela",
-                color = Grey
+            .size(
+                width = 376.dp,
+                height = 160.dp
             )
-            Row(
-                verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .clip(RoundedCornerShape(20.dp)),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            NetworkImage(
+                url = movie.poster,
+                contentDescription = movie.title,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .size(
+                        width = 102.dp,
+                        height = 138.dp
+                    )
+                    .clip(RoundedCornerShape(13.dp))
+            )
+            Column(
+                verticalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
             ) {
-                Icon(
-                    Icons.Outlined.Star,
-                    "star",
-                )
+                Column {
+                    Text(
+                        movie.title,
+                        color = Grey
+                    )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            Icons.Outlined.Star,
+                            "star",
+                            tint = Yellow
+                        )
+                        Text(
+                            movie.voteAverage.toString(),
+                            color = Yellow
+                        )
+                    }
+                }
                 Text(
-                    "8.9",
-                    color = Yellow
+                    "Graig Gillespie",
+                    color = Grey
                 )
             }
-            Text(
-                "Graig Gillespie",
-                color = Grey
-            )
         }
     }
 }
